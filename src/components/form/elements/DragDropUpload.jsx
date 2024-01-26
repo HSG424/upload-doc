87abb0ea5d457c731089e5212b06a3d3fa0cb4b2
HSG424 <mrhan998@gmail.com>
2024-01-26 08:06:19 -0600

    upload file styling

--- src/components/form/elements/DragDropUpload.jsx
+++ src/components/form/elements/DragDropUpload.jsx
@@ -1,13 +1,22 @@
+import document from "/document.svg";
+
 export const DragDropUpload = (props) => {
   return (
-    <div {...props.getRootProps()}>
+    <div {...props.getRootProps()} className="upload-container">
       <input {...props.getInputProps()} />
-      <p>Drag and drop files here or click to browse.</p>
-      <ul>
-        {props.uploadedFiles.map((file) => (
-          <li key={file.name}>{file.name}</li>
-        ))}
-      </ul>
+      <div className="upload-container-inner">
+        <div className="document-icon">
+          <img src={document} alt="Document icon" />
+        </div>
+        <p className="upload-msg">
+          Drag & Drop Here Or <span>Browse</span>
+        </p>
+        <ul>
+          {props.uploadedFiles.map((file) => (
+            <li key={file.name}>{file.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
